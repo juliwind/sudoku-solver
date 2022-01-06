fe2e00c06d6a7340e3e1c64e037be5adb4a815c5
juliwind <julianwindheuser@gmail.com>
2022-01-06 19:45:55 +0100

    image processing finsished

---
+++ main.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+from utils import *
+import pytesseract
+
+tesseract_path = 'C:\Program Files\Tesseract-OCR'
+pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# ---
+img_path = "Resources/sudoku1  .jpg"
+img_widht, img_height = 810, 810
+
+# ---
+# preprocess Image
+img = cv2.resize(cv2.imread(img_path), (img_widht, img_height))
+
+img_processed = cv2.adaptiveThreshold(cv2.GaussianBlur(
+    cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (5, 5), 1), 255, 1, 1, 11, 2)
+
+# find contours
+
+img_contours = img.copy()
+contours, hierarchy = cv2.findContours(
+    img_processed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 3)
+
+# find biggest contour
+img_biggestContour = img.copy()
+biggest_contour, biggest_area = defineBiggestContour(contours)
+if biggest_contour.size > 0:
+    biggest_contour = orderPoints(biggest_contour)
+    cv2.drawContours(img_biggestContour, biggest_contour, -1, (255, 0, 0), 20)
+
+# warp perspective
+    pts1 = np.float32(biggest_contour)
+    pts2 = np.float32(
+        [[0, 0], [img_widht, 0], [0, img_height], [img_widht, img_height]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    img_soduko = cv2.warpPerspective(
+        img, matrix, (img_widht, img_height))
+    img_soduko_black = cv2.cvtColor(img_soduko, cv2.COLOR_BGR2GRAY)
+
+# read the soduko
+all_fields = []
+rows = np.vsplit(img_soduko_black, 9)
+for i in rows:
+    columns = np.hsplit(i, 9)
+    for j in columns:
+        all_fields.append(j)
+
+# for i in all_fields:
+   # pytesseract.image_to_string(i)
+
+
+# output
+cv2.imshow("img", img)
+cv2.waitKey(0)
+
+cv2.imshow("processed", img_processed)
+cv2.waitKey(0)
+
+cv2.imshow("contours", img_contours)
+cv2.waitKey(0)
+
+cv2.imshow("biggest contour", img_biggestContour)
+cv2.waitKey(0)
+
+cv2.imshow("warp perspective", img_soduko)
+cv2.waitKey(0)
+
+cv2.imshow("warp perspective black", img_soduko_black)
+cv2.waitKey(0)
+
+for i in all_fields:
+    cv2.imshow("single field", i)
+    cv2.waitKey(0)
---
+++ utils.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+
+def defineBiggestContour(contours):
+    biggest_contour = []
+    biggest_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        if area > 50:
+            perimeter = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02*perimeter, True)
+            if area > biggest_area and len(approx) == 4:
+                biggest_contour = approx
+                biggest_area = area
+    return biggest_contour, biggest_area
+
+
+def orderPoints(points):
+    points = points.reshape((4, 2))
+    new_points = np.zeros((4, 1, 2), dtype=np.int32)
+    add = points.sum(1)
+    new_points[0] = points[np.argmin(add)]
+    new_points[3] = points[np.argmax(add)]
+    diff = np.diff(points, axis=1)
+    new_points[1] = points[np.argmin(diff)]
+    new_points[2] = points[np.argmax(diff)]
+    return new_points
